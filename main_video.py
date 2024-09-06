1dc0efdf655d6741642a539bba8e17d84e7683fc
mominwaqas15 <mominwaqas15@gmail.com>
2024-09-06 14:05:05 +0500

    By Momin (Minor changes & Ran Arham's script)

--- main_video.py
+++ main_video.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from helpers_shop import attempt_login
@@ -8,11 +9,22 @@ from helpers_video import scrape_video_details,scrape_500_video
 load_dotenv()
 
 def main():
-    chromedriver_path = r'C:\Users\arham\Desktop\chromedriver-win64\chromedriver.exe'
-    service = Service(chromedriver_path)
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(service=service, options=options)
+    # chromedriver_path = r'C:\Users\arham\Desktop\chromedriver-win64\chromedriver.exe'
+
+    service = Service(os.getenv("PATH_TO_CHROMEDRIVER"))
+
+    # service = Service(chromedriver_path)
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Enable headless mode
+    chrome_options.add_argument("--window-size=1920,1080")  # Set a large window size
+    chrome_options.add_argument("--start-maximized")  # Maximized in case it's not headless
+
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--start-maximized")
+    # driver = webdriver.Chrome(service=service, options=options)
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
         attempt_login(driver)
